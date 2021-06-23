e8ba0f570e6bd11cb42488de6fc19698503b4041
Alona-Mykhaylenko <79852771+Alona-Mykhaylenko@users.noreply.github.com>
2021-06-23 16:50:40 +0200

    refactored with redux toolkit

--- src/redux/app-actions.js
+++ src/redux/app-actions.js
@@ -1,31 +1,50 @@
 
+import { createAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 
 
-const ADDCONTACT = 'app/addContact';
-const HANDLECHANGE = 'app/handleChange';
-const HANDLEDELETE = 'app/handleDelete';
 
-export {ADDCONTACT, HANDLECHANGE, HANDLEDELETE};
+// const ADDCONTACT = 'app/addContact';
+// const HANDLECHANGE = 'app/handleChange';
+// const HANDLEDELETE = 'app/handleDelete';
 
-const addContact = (name, number) =>({
-    type: ADDCONTACT,
-    payload: {
-        id: uuidv4(),
-        name,
-        number,
-        filter: '',
-      }
+// export {ADDCONTACT, HANDLECHANGE, HANDLEDELETE};
+
+// const addContact = (name, number) =>({
+//     type: ADDCONTACT,
+//     payload: {
+//         id: uuidv4(),
+//         name,
+//         number,
+//         filter: '',
+//       }
+// })
+
+const addContact = createAction('app/addContact', (name, number) =>{
+    return {
+        payload: {
+            id: uuidv4(),
+            name,
+            number,
+            filter: '',
+          }
+
+    }
 })
 
-const handleDelete = (id) =>({
-    type: HANDLEDELETE,
-    payload: id
-})
+// const handleDelete = (id) =>({
+//     type: HANDLEDELETE,
+//     payload: id
+// })
 
-const handleChange = (ETvalue) =>({
-    type: HANDLECHANGE,
-    payload: ETvalue
-})
+const handleDelete = createAction('app/handleDelete')
+
+
+// const handleChange = (ETvalue) =>({
+//     type: HANDLECHANGE,
+//     payload: ETvalue
+// })
+
+const handleChange = createAction('app/handleChange')
 
 export {addContact, handleDelete, handleChange};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,45 +1,70 @@
-import { createStore, combineReducers } from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {ADDCONTACT, HANDLEDELETE, HANDLECHANGE} from './app-actions';
-
-// const InitialState = {
-//   contacts: [],
-//   filter: ""
-// }
+import { configureStore, createReducer, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+// import { combineReducers } from "redux";
+// import {ADDCONTACT, HANDLEDELETE, HANDLECHANGE} from './app-actions';
+// import actions from './app-actions'
+import {addContact, handleDelete, handleChange} from './app-actions';
 
 
 
-const ContactsReducer = (state = [], { type, payload })=>{
-  switch(type){
-    case ADDCONTACT:
-      
-      return [...state, payload];
 
-      case HANDLEDELETE:
-        return state.filter((contact) => contact.id !== payload);
+const middleware = [...getDefaultMiddleware(), logger];
 
-      default:
-        return state;
-  }
-  
-}
-
-const FilterReducer = (state = '', { type, payload })=>{
-  switch(type){
-    case HANDLECHANGE:
-      return payload;
-
-           default:
-        return state;
-  }
-}
-
-const reducer = combineReducers({
-  contacts:ContactsReducer,
-  filter: FilterReducer,
-
+const ContactsReducer = createReducer([], {
+  [addContact]:(state, {payload})=> [...state, payload],
+  [handleDelete]: (state, {payload}) => state.filter((contact) => contact.id !== payload),
 })
 
-const store = createStore(reducer, composeWithDevTools());
+
+// const ContactsReducer = (state = [], { type, payload })=>{
+//   switch(type){
+//     case ADDCONTACT:
+      
+//       return [...state, payload];
+
+//       case HANDLEDELETE:
+//         return state.filter((contact) => contact.id !== payload);
+
+//       default:
+//         return state;
+//   }
+  
+// }
+
+// const FilterReducer = (state = '', { type, payload })=>{
+//   switch(type){
+//     case 'app/handleChange':
+//       return payload;
+
+//            default:
+//         return state;
+//   }
+// }
+
+const FilterReducer = createReducer('', {
+  [handleChange]: (_, {payload})=> payload,
+})
+
+// const FilterReducer = createReducer([], {
+//   [handleChange]: (state, {payload}) => payload,
+// })
+
+
+// const reducer = combineReducers({
+//   contacts:ContactsReducer,
+//   filter: FilterReducer,
+
+// })
+
+// const store = createStore(reducer, composeWithDevTools());
+
+const store = configureStore({
+  reducer:{
+    contacts:ContactsReducer,
+    filter: FilterReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',})
+
 
 export default store;
